8cbd414def75b29c789a2a0735185470f57eb262
LuYee6813 <B11130233@gapps.ntust.edu.tw>
2023-05-11 20:58:37 +0800

    finshed logo-board menu-board and game-board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,33 @@
-function start(){
+var logoBoard = document.getElementById("logo-board");
+var menuBoard = document.getElementById("menu-board");
+var gameBoard = document.getElementById("game-board");
 
+function loadMenu(){
+    logoBoard.style.display = 'none';
+    menuBoard.style.display = 'block';
+    gameBoard.style.display = 'none';
+    console.log("111");
+}
+
+function start(){
+    console.log("開始遊戲");
+    logoBoard.style.display = 'none';
+    menuBoard.style.display = 'none';
+    gameBoard.style.display = 'block';
+}
+
+function online(){
+    console.log("線上模式");
+}
+
+function about(){
+    console.log("關於");
+}
+
+function options(){
+    console.log("其他選項");
 }
 
 function exit(){
-
+    console.log("離開");
 }
