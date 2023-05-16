ec0fa00670329aaf309f73932a8d9db52aee0a25
AllenTing <allen0523win@gmail.com>
2023-05-16 15:05:40 +0800

    add piece initialize position

--- src/core/Board.cpp
+++ src/core/Board.cpp
@@ -2,8 +2,8 @@
  *  File: Board.cpp
  *  Author: 張皓鈞(HAO) m831718@gmail.com
  *  Create Date: 2023/05/11 01:46:16
- *  Editor: 張皓鈞(HAO) m831718@gmail.com
- *  Update Date: 2023/05/13 18:18:02
+ *  Editor: 鄭健廷 (B11130225@mail.ntust.edu.tw)
+ *  Update Date: 2023/05/16 15:02:40
  *  Description: Board Class
  */
 
@@ -15,11 +15,38 @@ using namespace Chess;
 bool Board::initialize()
 {
     // Generate default board
+    // Generate pawn position
     for ( size_t i = 0; i < this->_width; ++i )
         this->_board[1][i] = new Pawn(TPlayer::kBlack);
 
     for ( size_t i = 0; i < this->_width; ++i )
         this->_board[6][i] = new Pawn(TPlayer::kWhite);
+
+    // Generate king position
+    this->_board[0][4] = new King(TPlayer::kBlack);
+    this->_board[7][4] = new King(TPlayer::kWhite);
+
+    // Generate queen position
+    this->_board[0][3] = new Queen(TPlayer::kBlack);
+    this->_board[7][3] = new Queen(TPlayer::kWhite);
+
+    // Generate rook position
+    this->_board[0][0] = new Rook(TPlayer::kBlack);
+    this->_board[0][7] = new Rook(TPlayer::kBlack);
+    this->_board[7][0] = new Rook(TPlayer::kWhite);
+    this->_board[7][7] = new Rook(TPlayer::kWhite);
+
+    // Generate bishop position
+    this->_board[0][2] = new Bishop(TPlayer::kBlack);
+    this->_board[0][5] = new Bishop(TPlayer::kBlack);
+    this->_board[7][2] = new Bishop(TPlayer::kWhite);
+    this->_board[7][5] = new Bishop(TPlayer::kWhite);
+
+    // Generate knight position
+    this->_board[0][1] = new Knight(TPlayer::kBlack);
+    this->_board[0][6] = new Knight(TPlayer::kBlack);
+    this->_board[7][1] = new Knight(TPlayer::kWhite);
+    this->_board[7][6] = new Knight(TPlayer::kWhite);
 
     return true;
 }
